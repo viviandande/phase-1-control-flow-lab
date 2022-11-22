4ccc94a6a96899114d716004815a979ec7b5b9fc
+ <+>
2022-11-22 17:37:27 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if(feet<= 400){
+    return 'This one is on me!'
+  }
+  else if (feet>2000 && feet<2500){
+    return 'I will gladly take your thirty bucks.'
+}
+  return 'No can do.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC'? 'Ok, sounds good.':'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+    return 'Thank you so much.'
+    case 'not as generous':
+    return 'Thank you.'
+
+    break;
+    default:
+    case 'if anything else':
+    return 'Bye.'
+  }
 }
