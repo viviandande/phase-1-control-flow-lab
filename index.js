27e7a7cdffff7f56193636c8a966dac8e4d922f0
+ <+>
2022-12-21 13:30:29 +0300

    update changes

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-function scuberGreetingForFeet(feet){
-  if(feet<= 400){
-    return 'This one is on me!'
+function scuberGreetingForFeet(feetTraveled) {
+  if(feetTraveled <= 400){
+      return "This one is on me!"
+  } else if(feetTraveled >= 2000 && feetTraveled <2500){
+      return "I will gladly take your thirty bucks."
+
+  }else{
+      return "No can do."
   }
-  else if (feet>2000 && feet<2500){
-    return 'I will gladly take your thirty bucks.'
-}
-  return 'No can do.';
 }
 
 function ternaryCheckCity(city){
-  return city === 'NYC'? 'Ok, sounds good.':'No go.'
+  return city === 'NYC' ? "Ok, sounds good." : "No go.";
 }
 
 function switchOnCharmFromTip(tip){
   switch(tip){
-    case 'generous':
-    return 'Thank you so much.'
-    case 'not as generous':
-    return 'Thank you.'
+      case "generous":
+      return "Thank you so much.";
+      break;
 
-    break;
-    default:
-    case 'if anything else':
-    return 'Bye.'
+      case "not as generous":
+      return  "Thank you.";
+      break;
+
+      default:
+      case "if anything else":
+      return "Bye.";
+
   }
 }
